29796e0d1c0c6c76107ae471b34631f7b4d7d930
SPECTR <ivanov8465@mail.ru>
2023-04-04 19:57:07 +0300

    Сделал 6 задание

---
+++ Task6/Program.cs
@@ -0,0 +1,34 @@
+﻿void WriteMatrix (int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      if (matrix[i,j] / 10 <= 0)
+      Console.Write($" {matrix[i,j]} ");
+
+      else Console.Write($"{matrix[i,j]} ");
+    }
+    Console.WriteLine();
+  }
+}
+Console.Clear();
+int n = 4;
+int[,] matrix = new int[n, n];
+int temp = 1;
+int i = 0;
+int j = 0;
+while (temp <= matrix.GetLength(0) * matrix.GetLength(1))
+{
+  matrix[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= matrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > matrix.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+WriteMatrix(matrix);
