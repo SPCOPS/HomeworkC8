01f66bccdbbdb3ffe34d33a80fb0c1cb607149ae
SPECTR <ivanov8465@mail.ru>
2023-04-04 19:56:55 +0300

    Сделал 5 задание

---
+++ Task5/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.Write("Введите количество ступенек треугольника Паскаля: ");
+string a = Console.ReadLine()!;
+int n = Convert.ToInt32(a);
+Console.WriteLine();
+for (int y = 0; y < n; y++)
+{
+    int b = 1;
+    for (int m = 0; m < n - y; m++)
+    {
+        Console.Write("   ");
+    }
+
+    for (int x = 0; x <= y; x++)
+    {
+        Console.Write("   {0:D} ", b);
+        b = b * (y - x) / (x + 1);
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+}
+Console.WriteLine();
