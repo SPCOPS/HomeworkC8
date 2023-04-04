bb733ef7ba3a8c81320d1d4d726cf726bfa5ef14
SPECTR <ivanov8465@mail.ru>
2023-04-04 19:56:24 +0300

    Сделал 2 задание

---
+++ Task2/Program.cs
@@ -0,0 +1,55 @@
+﻿Console.Clear();
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = GetMatrix(rows, columns, 0, 10);
+PrintMatrix(matrix);
+Console.WriteLine($"Строка с наименьшей суммой - {MinSum(matrix)}");
+int[,] GetMatrix(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return result;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int MinSum(int[,] matrix)
+{
+    int row = 0;
+    int minsum = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        minsum = minsum + matrix[0, i];
+    }
+
+    for (int i = 1; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum = sum + matrix[i, j];
+        }
+        if (minsum > sum)
+        {
+            minsum = sum;
+            row = i;
+        }
+    }
+    return row;
+}
